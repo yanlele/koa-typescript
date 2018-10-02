cd5096f2ba490402e3e20e8c64c7d8e789bf3315
yanlele <331393627@qq.com>
2018-10-02 21:50:11 +0800

    [+]整改配置文件

--- app/config.ts
+++ app/config.ts
@@ -1,6 +1,15 @@
 interface Config {
     redis: RedisConfig,
+    mysql: MysqlConfig
     port: number
+}
+
+interface MysqlConfig {
+    DATABASE: string,
+    USERNAME: string,
+    PASSWORD: string,
+    PORT: string,
+    HOST: string
 }
 
 interface RedisConfig {
@@ -16,6 +25,13 @@ const config: Config = {
         HOST: '127.0.0.1',
         DB: 1,
         TTL: 12 * 60 * 60 * 1000
+    },
+    mysql: {
+        DATABASE: '003_mmall_ts',
+        USERNAME: 'yanle',
+        PASSWORD: '123456',
+        PORT: '3306',
+        HOST: '127.0.0.1'
     },
     port: 3000
 };
