bacb6a91d2a9307558455afc0019fb6222e5d081
yanlele <331393627@qq.com>
2018-10-04 15:33:40 +0800

    [+]接口添加到请求路由中去

--- app/src/routes/user.ts
+++ app/src/routes/user.ts
@@ -7,6 +7,9 @@ router.prefix('/user');
 const userRouter = router
     .post('/signIn/',UserController.signIn)
     .get('/signOut/', UserController.signOut)
+    .post('/signUp/', UserController.signUp)
+    .get('/user-info/', UserController.getUserInfo)
+
 
 
 export default userRouter;
