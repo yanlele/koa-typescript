49a4fdaa48c5f7adb0446343808c2361eff2cbec
yanlele <331393627@qq.com>
2018-10-05 12:10:28 +0800

    [+]添加一个成功的测试用例实例

--- test/apis/getname.ts
+++
@@ -1,9 +0,0 @@
-import {expect} from 'chai'
-
-describe('routers: session', (): void => {
-
-    it('should create a session', function () {
-        expect(10).equal(10);
-    })
-
-});
--- test/index.test.ts
+++ test/index.test.ts
@@ -3,7 +3,20 @@
  * connect me 331393627@qq.com
  * create time 2018-10-05 11:30
  */
+import 'mocha';
+import {expect} from "chai";
 
-declare('test', function() {
 
-})
+describe('main', (): void => {
+    it('expect 10 = 10', function () {
+        expect(10).equal(10);
+        console.log(true);
+    });
+
+
+    describe('hello', function () {
+        it('test', function () {
+            expect(100).equal(100);
+        })
+    })
+});
